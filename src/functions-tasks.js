57cd5dd7e78d3f31b9e6e47482cd1983a4fb83a8
DialecticalLaw <d.shmuratkin@mail.ru>
2024-04-07 12:00:19 +0300

    init: start core-js-functions task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -18,7 +18,7 @@
  *
  */
 function getCurrentFunctionName() {
-  throw new Error('Not implemented');
+  return getCurrentFunctionName.name;
 }
 
 /**
@@ -32,8 +32,9 @@ function getCurrentFunctionName() {
  *   getFunctionBody(hiHello) => "function hiHello() { console.log('hello world'); }"
  *
  */
-function getFunctionBody(/* func */) {
-  throw new Error('Not implemented');
+function getFunctionBody(func) {
+  if (!func) return '';
+  return func.toString();
 }
 
 /**
@@ -50,8 +51,12 @@ function getFunctionBody(/* func */) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  const result = [];
+  funcs.forEach((func) => {
+    result.push(func.length);
+  });
+  return result;
 }
 
 /**
@@ -70,8 +75,10 @@ function getArgumentsCount(/* funcs */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return function (num) {
+    return num ** exponent;
+  };
 }
 
 /**
@@ -105,8 +112,15 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cachedNum;
+  return function () {
+    if (cachedNum === undefined) {
+      cachedNum = func();
+      return cachedNum;
+    }
+    return cachedNum;
+  };
 }
 
 /**
